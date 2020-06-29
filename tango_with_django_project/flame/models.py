90bf89b78902377292551ada411f40d024d428fc
purfect_k <sunhemi@hotmail.com>
2020-06-28 21:03:25 -0400

    Draft4
    Added some html files

--- tango_with_django_project/flame/models.py
+++ tango_with_django_project/flame/models.py
@@ -5,85 +5,96 @@ from django.utils import timezone
 # Create your models here.
 
 # Category Model
+
+
 class Category(models.Model):
-    name = models.CharField(max_length = 50)
+    name = models.CharField(max_length=50)
+
     class Meta:
-            verbose_name_plural = "Categories"
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
 
-# Tag Model 
+# Tag Model
+
+
 class Tag(models.Model):
-    name = models.CharField(max_length = 50)
+    name = models.CharField(max_length=50)
+
     def __str__(self):
         return self.name
 
-# pick from Hot Deals / Huo-Only Deals / Normal Deals 
+# pick from Hot Deals / Huo-Only Deals / Normal Deals
 
 # class Feature(models.Model):
-#    name = models.CharField(max_length = 12) 
+#    name = models.CharField(max_length = 12)
 #    # option = feature_option[2]
 #    def __str__(self):
 #        return self.name
 
+
 class Store(models.Model):
-    #store information 
-    name = models.CharField(max_length = 50)
-    address = models.CharField(max_length = 100,blank=True)
+    # store information
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100, blank=True)
     websites = models.URLField(blank=True)
     # phone number field - may change to a better one
-    phone = models.CharField(max_length = 10, blank=True)
-    wechat = models.CharField(max_length = 30, blank = True) 
+    phone = models.CharField(max_length=10, blank=True)
+    wechat = models.CharField(max_length=30, blank=True)
 
     def __str__(self):
-        return self.name 
+        return self.name
 
-# Add a post status 
+# Add a post status
 
 
 class Deal(models.Model):
     STATUS = (
-    ('draft', "Draft"),
-    ('publish',"Publish")
+        ('draft', "Draft"),
+        ('publish', "Publish")
     )
 
     FEATURE = (
-    ('HotDeals', 'Hot Deals'),
-    ('HuoOnly', 'Huozhezi Only'),
-    ('Trending', 'Trending Deals'),
-    ('GoodDeals','Good Deals'),
+        ('HotDeals', 'Hot Deals'),
+        ('HuoOnly', 'Huozhezi Only'),
+        ('Trending', 'Trending Deals'),
+        ('GoodDeals', 'Good Deals'),
     )
 
-    # Basic Properties 
-    title = models.CharField(max_length = 200)
+    # Basic Properties
+    title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
 
-    ###############!!! NEED TO ADD IMAGE TOO 
-    content = models.TextField() 
-    author = models.ForeignKey(User,on_delete= models.CASCADE,related_name='blog_posts')
+    content = models.TextField(max_length=200)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
     publish_time = models.DateTimeField(default=timezone.now)
-    not_delated = models.BooleanField(default = True)
-    is_draft = models.CharField(max_length=10, choices=STATUS, default='publish')
-    feature = models.CharField(max_length=10,choices = FEATURE, default='GoodDeals')    
+    not_delated = models.BooleanField(default=True)
+    is_draft = models.CharField(
+        max_length=10, choices=STATUS, default='publish')
+    feature = models.CharField(
+        max_length=10, choices=FEATURE, default='GoodDeals')
 
-    
-    # Customized Properties 
+    # Customized Properties
     effective_date = models.DateField()
     expire_date = models.DateField()
     is_active = models.BooleanField(default=True)
 
-    old_price = models.DecimalField(max_digits=10, decimal_places=2,blank=True,default=0)
-    new_price = models.DecimalField(max_digits=10, decimal_places=2,blank=True,default=0)
+    old_price = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, default=0)
+    new_price = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, default=0)
     #save = "{:.0f}".format((new_price - old_price)/old_price)
 
-    # Save Deal Information 
-    store = models.ForeignKey(Store, on_delete=models.CASCADE,related_name="store",null = True)
-        #url info: if there is a link: putlink 
-        # o/w put deal itself default link
+    # Save Deal Information
+    store = models.ForeignKey(
+        Store, on_delete=models.CASCADE, related_name="store", null=True)
+    # url info: if there is a link: putlink
+    # o/w put deal itself default link
     url = models.URLField(blank=True)
-
     # views = models.IntegerField(max_length = 2)
     # likes = models.IntergerField(max_length = 2)
 
@@ -92,5 +103,3 @@ class Deal(models.Model):
 
     def __str__(self):
         return self.title
-
-
--- tango_with_django_project/flame/views.py
+++ tango_with_django_project/flame/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.template import loader 
+from django.template import loader
 from .models import Deal, Store
 
 # Create your views here.
 
+
 def index(request):
     latest_deal_list = Deal.objects.order_by('-publish_time')[:5]
     template = loader.get_template('index.html')
-    context = { 'latest_deal_list': latest_deal_list}
+    context = {'latest_deal_list': latest_deal_list}
     return HttpResponse(template.render(context, request))
+
 
 def about(request):
     return HttpResponse("This is About Page")
 
+
 def deal(request, deal_id):
-    deal = get_object_or_404(Deal, pk = deal_id)
+    deal = get_object_or_404(Deal, pk=deal_id)
     template = 'deal.html'
     return render(request, template, {'deal': deal})
 
@@ -23,7 +26,8 @@ def deal(request, deal_id):
 def details(request, deal_id):
     return HttpResponse("you are looking on Deal Number: %s " % deal_id)
 
+
 def store(request, store_id):
-    store = get_object_or_404(Store, pk = store_id)
+    store = get_object_or_404(Store, pk=store_id)
     template = 'store.html'
     return render(request, template, {'store': store})
